4a456779a8bf60b5a7bc66b74ce6aa4db5226271
rushikadivar <rushikadivar2701@gmail.com>
2021-06-07 00:58:24 +0530

    send to multiple recipient

--- nodemailer/index.js
+++ nodemailer/index.js
@@ -3,6 +3,7 @@ const ejs = require('ejs');
 const nodemailer = require('nodemailer');
 const path = require('path');
 // const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const app = express();
 
@@ -23,13 +24,33 @@ app.get('/', (req, res) => {
 
 app.post('/send', (req, res) => {
 
-    let maillist = [
-        'xyz@gmail.com',
-        'abc@gmail.com',
-        'abcagain@gmail.com'
-    ];
+  var obj = [];
+  async function mailer() {
+    let result = await fetch('http://localhost:5000/user/').then((res) => res.json());   
+    // let data = await result;
 
-    maillist.toString();
+    let data = JSON.stringify(result);
+    let useData = JSON.parse(data);
+
+    for (let i in useData) {
+      obj.push(useData[i].email)
+    }
+    console.log(obj.toString());
+    // if (!err) {
+
+    // }
+    res.redirect('/confirmation');
+
+  }
+  mailer();
+
+    // let maillist = [
+    //     'xyz@gmail.com',
+    //     'abc@gmail.com',
+    //     'abcagain@gmail.com'
+    // ];
+
+    // maillist.toString();
 
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -43,10 +64,15 @@ app.post('/send', (req, res) => {
     
       transporter.sendMail({
         from: req.body.from, 
-        to: maillist, 
+        to: obj, 
         subject: "Hello ✔", 
         text: req.body.Body, 
         html: req.body.Body,
+        attachments: [
+          {
+            filename: 'Appointment_slip (1).pdf', path: './nature.jpeg'
+          }
+        ]
       }, (err, result) => {
         if (err) {
           console.log(err);
